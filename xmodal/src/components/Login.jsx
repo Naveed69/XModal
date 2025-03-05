1bb2928624be8fc1cd323164be0f6b8e846c39f2
naveed69 <naveed.desai.69@gmail.com>
2025-03-05 20:17:50 +0530

    added style

--- xmodal/src/components/Login.jsx
+++ xmodal/src/components/Login.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import styles from "./Login.module.css";
-import ModalSet from "./Modal/ModalSet";
+import ModalSet from "./Modal/Modal";
 const Login = () => {
   const [openModal, setOpenModal] = useState(false);
+
   return (
     <>
       <h1>User Details Modal</h1>
-      <button onClick={() => setOpenModal(true)}> Open Form</button>
-      <ModalSet open={openModal} children={<>Hello</>} />
+      <button onClick={() => setOpenModal(true)}>Open Form</button>
+      <ModalSet isOpen={openModal} setIsOpen={setOpenModal}>
+        {<h1>Hello</h1>}
+      </ModalSet>
     </>
   );
 };
